8be7d03c978c41e87373e60fcc705fe69577388c
채정실 <cowjdtlf@gmail.com>
2020-07-13 20:54:43 +0900

    Gradle 활용...  문법은 맞는데 왜 안될까

--- app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
@@ -3,7 +3,9 @@ package kr.co.tjoeun.library_20200713
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile_photo.*
 
 class MainActivity : BaseActivity() {
 
@@ -27,6 +29,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val imgUrl = "https://t1.daumcdn.net/cfile/tistory/99AADE425EA423D021"
+        Glide.with(mContext).load(imgUrl).into(profileImg)
 
     }
 
