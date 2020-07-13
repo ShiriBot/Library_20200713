a775697f5d51ed919dac7d700b2be26d9cdf2be3
채정실 <cowjdtlf@gmail.com>
2020-07-13 22:21:29 +0900

    Permission Listener 가이드라인까지

--- app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_profile_photo.*
+import java.security.Permission
 
 class MainActivity : BaseActivity() {
 
@@ -24,13 +27,25 @@ class MainActivity : BaseActivity() {
 
         callBtn.setOnClickListener {
 
-            val phoneNum = phoneNumTxt.text.toString()
 
-            val myUri = Uri.parse("tel:${phoneNum}")
 
-            val myIntent = Intent(Intent.ACTION_CALL,myUri)
-            startActivity(myIntent)
+            val permissionListener = object : PermissionListener{
+                override fun onPermissionGranted() {
+//                    승인을 받은 상황.=> 실제로 전화를 걸자
+                    val phoneNum = phoneNumTxt.text.toString()
 
+                    val myUri = Uri.parse("tel:${phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    최종 거부된 상황. 통화를 할 수 없다.
+
+                    Toast.makeText(mContext,"통화권한이 거부되어 연결이 불가합니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
 
         }
 
